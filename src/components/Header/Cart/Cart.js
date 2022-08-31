c11853efd6a5e75440cd38bcd8c450deb4e68dbc
김진영 <jinyoung01099@gmail.com>
2022-08-31 20:15:31 +0900

    [Add] 장바구니 레이아웃 구현

--- src/components/Header/Cart/Cart.js
+++ src/components/Header/Cart/Cart.js
@@ -1,38 +1,43 @@
+import { useState } from 'react';
 import './Cart.scss';
 import Modal from 'react-modal';
 
 export default function Cart({ setIsCartClicked }) {
+  const arr = [1, 2, 3, 4];
+
+  const [itemData, setItemData] = useState(
+    arr.map(() => {
+      return (
+        <div className="item_box">
+          <img src="./Images/home_part_1.jpg" alt="item" className="pic" />
+          <div className="text_box">
+            <div className="title">로이시 시그니처 초콜릿</div>
+            <div className="price">₩ 32,000</div>
+            <div className="wrapper">
+              <img src="./Images/add.png" alt="add" className="add" />
+              <p className="quantity">3</p>
+              <img src="./Images/minus.png" alt="minus" className="minus" />
+              <div className="remove">제거</div>
+            </div>
+          </div>
+        </div>
+      );
+    })
+  );
+
   return (
-    <div>
-      <Modal
-        isOpen={true}
-        onRequestClose={() => setIsCartClicked(false)}
-        className="modal flex_center"
-      >
-        <div className="modal_top">imtop</div>
-        <div>ddd</div>
-        <div>ddd</div>
-        <div>ddd</div>
-        <div>ddd</div>
-        <div>ddd</div>
-        <div>ddd</div>
-        <div>ddd</div>
-        <div>ddd</div>
-        <div>ddd</div>
-        <div>ddd</div>
-        <div>ddd</div>
-        <div>ddd</div>
-        <div>ddd</div>
-        <div>ddd</div>
-        <div>ddd</div>
-        <div>ddd</div>
-        <div>ddd</div>
-        <div>ddd</div>
-        <div>ddd</div>
-        <div>ddd</div>
-        <div>ddd</div>
-        <div className="modal_bottom">imbottom</div>
-      </Modal>
-    </div>
+    <Modal
+      isOpen={true}
+      onRequestClose={() => setIsCartClicked(false)}
+      style={{ outline: 'none' }}
+      className="modal "
+    >
+      {itemData}
+
+      <div className="modal_bottom flex_center">
+        <p className="total_price">총 금액 : ₩ 184,000</p>
+        <div className="buy_btn flex_center">구매하기</div>
+      </div>
+    </Modal>
   );
 }
