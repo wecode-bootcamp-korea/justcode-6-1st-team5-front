172c610dd3d248a49f4f536b705945ff34d5fa6f
jeong-yewon <jdc209802@gmail.com>
2022-08-31 14:53:05 +0900

    add) detail page 일부 기능 구현

---
+++ src/pages/Detail/detail-components/Photo/PhotoList.js
@@ -0,0 +1,19 @@
+import React from 'react';
+
+import './PhotoList.scss';
+
+const PhotoList = () => {
+  return (
+    <>
+      <img src="image/초코 1.png" />
+      <img src="image/초코 2.png" />
+      <img src="image/초코 3.png" />
+      <img src="image/초코 4.png" />
+      <img src="image/초코 5.png" />
+      <img src="image/초코 6.png" />
+      <img src="image/초코 7.png" />
+    </>
+  );
+};
+
+export default PhotoList;
