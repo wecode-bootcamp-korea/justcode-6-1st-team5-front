a0ce8ccfe35f9d8a0c1e052aa5759c4e0433eda1
천찬영 <petcarplan@gmail.com>
2022-09-01 22:35:50 +0900

    [Add]라우터 추가

--- src/index.js
+++ src/index.js
@@ -1,10 +1,12 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import App from './App';
+import Main from './pages/Shop/Main';
 import Router from './pages/Router';
 
 import './styles/reset.scss';
 import './styles/common.scss';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
+
 root.render(<Router />);
