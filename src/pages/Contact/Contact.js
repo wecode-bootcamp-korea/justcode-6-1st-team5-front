5e61ef5330dffcc692fe187a8df21cbeb21f743c
SeanKim05 <103613612+SeanKim05@users.noreply.github.com>
2022-09-02 20:40:41 +0900

    [Add] contact 추가

---
+++ src/pages/Contact/Contact.js
@@ -0,0 +1,13 @@
+import React from 'react';
+
+function Contact() {
+  return (
+    <div>
+      <section className="wrapper_content_login">
+        <div>dasdasdas</div>
+      </section>
+    </div>
+  );
+}
+
+export default Contact;
--- src/pages/Signup/Signup.js
+++ src/pages/Signup/Signup.js
@@ -179,5 +179,4 @@ function Signup() {
     </section>
   );
 }
-
 export default Signup;
