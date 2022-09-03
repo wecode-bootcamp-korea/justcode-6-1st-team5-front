14724f80887213e9fb78c687ddc1fcb05c27b95c
SeanKim05 <103613612+SeanKim05@users.noreply.github.com>
2022-09-03 17:02:28 +0900

    [Add] Contact page 기초 작업 완료

--- src/pages/Contact/Contact.js
+++ src/pages/Contact/Contact.js
@@ -4,6 +4,7 @@ import './Contact.scss';
 function Contact() {
   return (
     <div className="">
+      <title className="contact_header_titile">CONTACT US FOR ASSISTANCE</title>
       <section className="wrapper_content_contact">
         <div className="contact_description">
           <h1 className="contact_description_h1">Need help?</h1>
@@ -12,13 +13,16 @@ function Contact() {
             alt="contact img"
             src="https://images.unsplash.com/photo-1610450949065-1f2841536c88?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=774&q=80"
           />
-          <div>
+          <div className="description_text">
             <p>We're here to answer your questions</p>
             <p>
               Please fill out this form for all products, order,shipping, and
               website related inquiries
             </p>
-            <p>Customer Care Team is available Monday to Friday, 9am to 5pm</p>
+            <p>
+              Customer Care Team is available Monday to Friday, 9am to 5pm ET.
+              We are closed on major US holiday
+            </p>
             <p>
               Thank you for visiting <span>ROECY'</span>website
             </p>
@@ -26,14 +30,35 @@ function Contact() {
         </div>
 
         <form className="contact_form">
-          <label htmlFor="name">Your name</label>
-          <input id="name" type="text" placeholder="Your name" />
+          <label className="contact_form_label" htmlFor="name">
+            Your name
+          </label>
+          <input
+            className="contact_form_input"
+            id="name"
+            type="text"
+            placeholder="Your name"
+          />
 
-          <label htmlFor="email">Your email</label>
-          <input id="email" type="text" placeholder="Your email" />
+          <label className="contact_form_label" htmlFor="email">
+            Your email
+          </label>
+          <input
+            className="contact_form_input"
+            id="email"
+            type="text"
+            placeholder="Your email"
+          />
 
-          <label htmlFor="message">Your message</label>
-          <input id="message" type="text" placeholder="Your message" />
+          <label className="contact_form_label" htmlFor="message">
+            Your message
+          </label>
+          <input
+            className="contact_form_input_lg"
+            id="message"
+            type="text"
+            placeholder="Your message"
+          />
           <button className="contact_btn">Send message</button>
         </form>
       </section>
