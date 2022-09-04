9d7e666c7020c8985b696504ad194811ed369bcd
천찬영 <petcarplan@gmail.com>
2022-09-05 00:09:39 +0900

    [Add]nc slider라이브러리 설치.

--- src/pages/Shop/CollectionInner/datafilteringtool/filteringkeyword/Price.js
+++ src/pages/Shop/CollectionInner/datafilteringtool/filteringkeyword/Price.js
@@ -1,6 +1,7 @@
-// import { useEffect, useState } from 'react';
+import Slider from 'rc-slider';
+import 'rc-slider/assets/index.css';
 
-function Price() {
+export default function Price() {
   return (
     <div id="price_filtering_category">
       <div className="button_style">
@@ -9,10 +10,11 @@ function Price() {
         <input className="pricebox" placeholder="106" />
       </div>
       <div id="pricenavibase">
-        <div id="priceline"></div> <div id="pricenavigator"></div>
+        <div id="priceline">
+          <Slider range />
+        </div>
         <div id="bottomlinebox" className="margin_right"></div>
       </div>
     </div>
   );
 }
-export default Price;
