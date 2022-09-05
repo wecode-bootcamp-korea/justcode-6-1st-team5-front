24ee2981dbc804f0e54876caff8066eec465c6f3
SeanKim05 <103613612+SeanKim05@users.noreply.github.com>
2022-09-05 15:08:18 +0900

    [Add] token 값에 따른 페이지 표시

--- src/components/Footer/Logout/Logout.js
+++ src/components/Footer/Logout/Logout.js
@@ -1,14 +1,26 @@
 import React from 'react';
 import './Logout.scss';
+import { useNavigate } from 'react-router-dom';
 
 function Logout() {
   const handleLogout = () => {
     localStorage.clear();
     // window.location.reload();
   };
+  const navigate = useNavigate();
+  const go_main = () => {
+    navigate('/');
+  };
   return (
     <div>
-      <button className="logout_btn" onClick={handleLogout}>
+      <button
+        className="logout_btn"
+        onClick={() => {
+          handleLogout();
+          go_main();
+        }}
+        // onMouseDown={go_main}
+      >
         Logout
       </button>
     </div>
--- src/pages/Login/Login.js
+++ src/pages/Login/Login.js
@@ -1,5 +1,6 @@
 import React, { useState, useEffect, useRef } from 'react';
 import { useNavigate } from 'react-router-dom';
+import Account from '../Account/Account';
 
 import './Login.scss';
 
@@ -51,11 +52,15 @@ function Login() {
       });
   };
 
-  // const getToken = () => {
-  //   if (localStorage.getItem('token') !== '') {
-  //     console.log('token');
-  //   }
-  // };
+  const tokenStatus = localStorage.getItem('token');
+
+  const getToken = () => {
+    if (tokenStatus) {
+      tokenStatus !== null ? setLoggedIn(true) : setLoggedIn(false);
+      // console.log(tokenStatus);
+      // setLoggedIn(false);
+    }
+  };
 
   useEffect(() => {
     setValidPwd(PWD_REGEX.test(pwd));
@@ -65,83 +70,93 @@ function Login() {
     setValidEmail(EMAIL_REGEX.test(email));
   }, [email]);
 
-  // useEffect(() => {
-  //   getToken();
-  // }, []);
+  useEffect(() => {
+    getToken();
+  }, []);
 
   return (
-    <section className="wrapper_content_login">
-      <div className="border_box_login">
-        <div className="header_content_login">
-          <h1 className="login-text-h">Login</h1>
-          <p className="login-text-p">Please enter your e-mail password:</p>
-        </div>
+    <>
+      {loggedIn ? (
+        <Account />
+      ) : (
+        <section className="wrapper_content_login">
+          <div className="border_box_login">
+            <div className="header_content_login">
+              <h1 className="login-text-h">Login</h1>
+              <p className="login-text-p">Please enter your e-mail password:</p>
+            </div>
 
-        <div className="input_content_login">
-          <input
-            className="login_input"
-            placeholder="Email"
-            type="text"
-            ref={emailRef}
-            onChange={e => setEmail(e.target.value)}
-            required
-            aria-invalid={validEmail ? 'false' : 'true'}
-            aria-describedby="emailnote"
-            onFocus={() => setEmailFocus(true)}
-            onBlur={() => setEmailFocus(false)}
-          />
-          <p
-            id="emailnote"
-            className={
-              emailFocus && email && !validEmail ? 'cond_msg' : 'offscreen'
-            }
-          >
-            E-mail should include "@"
-            <br />
-            Please check your email address
-          </p>
+            <div className="input_content_login">
+              <input
+                className="login_input"
+                placeholder="Email"
+                type="text"
+                ref={emailRef}
+                onChange={e => setEmail(e.target.value)}
+                required
+                aria-invalid={validEmail ? 'false' : 'true'}
+                aria-describedby="emailnote"
+                onFocus={() => setEmailFocus(true)}
+                onBlur={() => setEmailFocus(false)}
+              />
+              <p
+                id="emailnote"
+                className={
+                  emailFocus && email && !validEmail ? 'cond_msg' : 'offscreen'
+                }
+              >
+                E-mail should include "@"
+                <br />
+                Please check your email address
+              </p>
 
-          <input
-            className="login_input"
-            type="password"
-            placeholder="Password"
-            ref={pwdRef}
-            onChange={e => setPwd(e.target.value)}
-            required
-            aria-invalid={validPwd ? 'false' : 'ture'}
-            aria-describedby="pwdnote"
-            onFocus={() => setPwdFocus(true)}
-            onBlur={() => setPwdFocus(false)}
-          />
-          <p
-            id="pwdnote"
-            className={pwdFocus && pwd && !validPwd ? 'cond_msg' : 'offscreen'}
-          >
-            Password length should be 8 to 24 characters. Must includes
-            <br />
-            uppercase, lowercase and one special characters(!,@,#,$).
-            <br />
-          </p>
+              <input
+                className="login_input"
+                type="password"
+                placeholder="Password"
+                ref={pwdRef}
+                onChange={e => setPwd(e.target.value)}
+                required
+                aria-invalid={validPwd ? 'false' : 'ture'}
+                aria-describedby="pwdnote"
+                onFocus={() => setPwdFocus(true)}
+                onBlur={() => setPwdFocus(false)}
+              />
+              <p
+                id="pwdnote"
+                className={
+                  pwdFocus && pwd && !validPwd ? 'cond_msg' : 'offscreen'
+                }
+              >
+                Password length should be 8 to 24 characters. Must includes
+                <br />
+                uppercase, lowercase and one special characters(!,@,#,$).
+                <br />
+              </p>
 
-          <button
-            type="button"
-            className="login_btn"
-            onClick={go_main}
-            onMouseDown={sendHandler}
-            disabled={!validPwd || !validEmail ? true : false}
-          >
-            Login
-          </button>
-        </div>
+              <button
+                type="button"
+                className="login_btn"
+                onClick={go_main}
+                onMouseDown={sendHandler}
+                disabled={!validPwd || !validEmail ? true : false}
+              >
+                Login
+              </button>
+            </div>
 
-        <div className="link_content_login">
-          <span className="span_link login_span">Don't have an account? </span>
-          <span className="login_span" onClick={go_signup}>
-            Create one
-          </span>
-        </div>
-      </div>
-    </section>
+            <div className="link_content_login">
+              <span className="span_link login_span">
+                Don't have an account?{' '}
+              </span>
+              <span className="login_span" onClick={go_signup}>
+                Create one
+              </span>
+            </div>
+          </div>
+        </section>
+      )}
+    </>
   );
 }
 
