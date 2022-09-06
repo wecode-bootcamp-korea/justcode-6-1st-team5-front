58530716a7ef91c52b737cf9f0d60e7dc3d76361
SeanKim05 <103613612+SeanKim05@users.noreply.github.com>
2022-09-06 19:46:47 +0900

    [Add] Account 이름 표시 구현

--- src/pages/Account/Account.js
+++ src/pages/Account/Account.js
@@ -1,17 +1,41 @@
 import React from 'react';
 import './Account.scss';
-import { useEffect } from 'react';
+import { useState, useEffect } from 'react';
 
 import Logout from '../../components/Logout/Logout';
 
 function Account() {
+  const [userName, setUserName] = useState([]);
+
+  useEffect(() => {
+    fetch('http://localhost:10010/users/name', {
+      method: 'POST',
+      headers: {
+        'Content-Type': 'application/json',
+      },
+      body: JSON.stringify({
+        token: localStorage.getItem('token'),
+      }),
+    })
+      .then(res => res.json())
+      .then(data => setUserName(data.userName));
+  }, []);
+
   return (
     <>
       <div className="wrapper_content_account">
         <section className="account_header">
           <Logout />
           <h1 className="account_header_title"> My Account</h1>
-          <p className="account_header_text">Welcome back!</p>
+          <p className="account_header_text">
+            Welcome back,
+            {userName.map(userName => (
+              <span className="account_header_text_username" key={0}>
+                {userName.name}
+              </span>
+            ))}
+            !
+          </p>
         </section>
 
         <section className="account_order">
